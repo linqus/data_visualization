f1aaec70d081b36c084bfab1e3e36c2bddbe5a79
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-07 19:58:00 +0200

    wartości z listy składanej, własny kolor punktów, własne zakresy osi

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,15 +1,18 @@
 import matplotlib.pyplot as plt
 
-x_values = [1,2,3,4,5]
-y_values = [1,4,9,16,25]
+x_values = range(1,1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 
-ax.scatter(x_values,y_values, s=200)
+ax.scatter(x_values,y_values, s=10, c=(0,0.8,0))
 ax.set_title('Scatter plot')
 ax.set_xlabel('Wartości',fontsize=14)
 ax.set_ylabel('Kwadraty wartości',fontsize=14)
-ax.tick_params(axis='both',which='major',labelsize=14)
+ax.tick_params(axis='both',which='major',labelsize=9)
+
+ax.axis([0,1100,0,1100000])
+
 
 plt.show()
