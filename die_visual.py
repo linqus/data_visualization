2b06c4ac77efdc398ab69ab4878f652cbee995c2
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-08 19:29:14 +0200

    histogram rzutu jedną kostką D6

--- die_visual.py
+++ die_visual.py
@@ -1,4 +1,7 @@
 from die import Die
+from plotly.graph_objs import Bar, Layout
+from plotly import offline 
+
 
 die = Die()
 results = []
@@ -9,4 +12,14 @@ frequencies = []
 for i in range(1,die.sides+1):
 	frequencies.append(results.count(i))
 
-print(frequencies)
+x_values = list(range(1,die.sides+1))
+data = [Bar(x=x_values,y=frequencies)]
+
+
+my_layout = Layout(
+				title="Wynik tysiąca rzutów kostką D6", 
+				xaxis={'title':'Wynik'},
+				yaxis={'title':'Częstość wyniku'}
+			)
+
+offline.plot({'data':data, 'layout':my_layout},filename='d6.html')
