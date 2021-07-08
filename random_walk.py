10aded0de16230386cf1a55c21698af7384e1745
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-08 17:55:36 +0200

    refaktoryzacja fill_walk() w klasie RandomWalk

--- random_walk.py
+++ random_walk.py
@@ -11,20 +11,19 @@ class RandomWalk:
 	def fill_walk(self,steps=100):
 
 		while len(self.x_values) < self.num_points:
-			x_direction = choice([1,-1])
-			x_step = choice([0,1,2,3,4])
 
-			y_direction = choice([1,-1])
-			y_step = choice([0,1,2,3,4])
+			x_step = self.get_step()
+			y_step = self.get_step()
 			
 			if x_step == 0 and y_step == 0:
 				continue
-
-			x_move = x_step * x_direction 
-			x = self.x_values[-1] + x_move
 			
-			y_move = y_step * y_direction
-			y = self.y_values[-1] + y_move
+			self.x_values.append(self.x_values[-1] + x_step)
+			self.y_values.append(self.y_values[-1] + y_step)
 
-			self.x_values.append(x)
-			self.y_values.append(y)
+	def get_step(self):
+		x_direction = choice([1,-1])
+		x_move = choice([0,1,2,3,4])
+		return  x_move * x_direction 
+
+
