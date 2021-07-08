1f04fa64c3a2397be70bca0b4a92fa7c7e86ff25
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-08 18:56:21 +0200

    częstości występowania rzutów - podejście nieoptymalne :)

--- die_visual.py
+++ die_visual.py
@@ -2,7 +2,14 @@ from die import Die
 
 die = Die()
 results = []
-for x in range(100):
+for x in range(1000):
 	results.append(die.roll())
 
-print(results)
+frequencies = []
+for i in range(1,die.sides+1):
+	frequencies.append(0)
+
+for n in results:
+	frequencies[n-1] += 1
+
+print(frequencies)
