2b560aa204ef66e5276dd9aa7b9602b96e167f60
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-08 19:50:24 +0200

    histogram rzutu dwiema koścmi D6 - wersja moja

--- die_visual.py
+++ die_visual.py
@@ -3,23 +3,24 @@ from plotly.graph_objs import Bar, Layout
 from plotly import offline 
 
 
-die = Die()
+die1 = Die()
+die2 = Die()
 results = []
 for x in range(1000):
-	results.append(die.roll())
+	results.append(die1.roll()+die2.roll())
 
 frequencies = []
-for i in range(1,die.sides+1):
+for i in range(1,die1.sides+die2.sides+1):
 	frequencies.append(results.count(i))
 
-x_values = list(range(1,die.sides+1))
+x_values = list(range(1,die1.sides+die2.sides+1))
 data = [Bar(x=x_values,y=frequencies)]
 
 
 my_layout = Layout(
-				title="Wynik tysiąca rzutów kostką D6", 
+				title="Wynik tysiąca rzutów dwiema kostkami D6", 
 				xaxis={'title':'Wynik'},
 				yaxis={'title':'Częstość wyniku'}
 			)
 
-offline.plot({'data':data, 'layout':my_layout},filename='d6.html')
+offline.plot({'data':data, 'layout':my_layout},filename='2xd6.html')
