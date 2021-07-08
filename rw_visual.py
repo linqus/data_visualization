18dbd6d28dd75ca08fe45e5aedd88c76d499721b
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-08 16:31:03 +0200

    punkt początkowy i końcowy

--- rw_visual.py
+++ rw_visual.py
@@ -10,7 +10,9 @@ while True:
 	rw = RandomWalk()
 	rw.fill_walk()
 
-	ax.scatter(rw.x_values,rw.y_values,s=7,c=range(1,5001),cmap=plt.cm.hot)
+	ax.scatter(rw.x_values,rw.y_values,s=15,edgecolors='none',c=range(1,5001),cmap=plt.cm.hot)
+	ax.scatter(rw.x_values[0],rw.y_values[0],s=100,c='green',edgecolors='none')
+	ax.scatter(rw.x_values[-1],rw.y_values[-1],s=100,c='red',edgecolors='none')
 
 	plt.show()
 	ending = input("Press 'n' to quit, any other key to continue")
