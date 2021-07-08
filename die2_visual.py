927b6f9e08ad36dfb192fe8f7ad4953c17172069
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-08 22:53:40 +0200

    #15-9 listy składane zamiast pętli

--- die2_visual.py
+++ die2_visual.py
@@ -6,14 +6,11 @@ from plotly import offline
 die1 = Die(8)
 die2 = Die(8)
 results = []
-num_rolls = 10_000_000
+num_rolls = 100_000
 max_rolls = die1.sides+die2.sides
-for x in range(num_rolls):
-	results.append(die1.roll()+die2.roll())
 
-frequencies = []
-for i in range(2,max_rolls+1):
-	frequencies.append(results.count(i))
+results = [die1.roll()+die2.roll() for x in range(num_rolls)]
+frequencies = [results.count(i) for i in range(2,max_rolls+1)]
 
 x_values = list(range(2,max_rolls+1))
 data = [Bar(x=x_values,y=frequencies)]
