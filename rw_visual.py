386639614d3f34a024bbaaaa59576212af78fe66
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-08 17:28:52 +0200

    osie, wielkość wykresu

--- rw_visual.py
+++ rw_visual.py
@@ -6,13 +6,16 @@ from random_walk import RandomWalk
 
 
 while True:
-	fig, ax = plt.subplots()
-	rw = RandomWalk()
+	fig, ax = plt.subplots(figsize=(10,8))
+	rw = RandomWalk(5_000)
 	rw.fill_walk()
 
-	ax.scatter(rw.x_values,rw.y_values,s=15,edgecolors='none',c=range(1,5001),cmap=plt.cm.hot)
+	ax.scatter(rw.x_values,rw.y_values,s=15,edgecolors='none',c=range(rw.num_points),cmap=plt.cm.plasma)
 	ax.scatter(rw.x_values[0],rw.y_values[0],s=100,c='green',edgecolors='none')
 	ax.scatter(rw.x_values[-1],rw.y_values[-1],s=100,c='red',edgecolors='none')
+
+	ax.get_xaxis().set_visible(False)
+	ax.get_yaxis().set_visible(False)
 
 	plt.show()
 	ending = input("Press 'n' to quit, any other key to continue")
