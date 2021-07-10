0227193570e9b1e3159b083e692ab38b796484a0
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-10 13:55:40 +0200

    wykres najniższych i najwyższych dziennych temperatur w 2018 w Sitka

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,9 +1,26 @@
 import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
+	dates, lows, highs = [], [],[]
+	for row in reader:
+		dates.append(datetime.strptime(row[2],"%Y-%m-%d"))
+		highs.append(int( row[5] ))
+		lows.append(int( row[6] ))
 
-print(header_row)
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+
+ax.plot(dates,highs,c='Red')
+ax.plot(dates,lows,c='Blue')
+ax.set_title('Dzienne temperatury w 2018 w Sitka')
+ax.set_xlabel('',fontsize=14)
+ax.set_ylabel('Temperatura (F)',fontsize=14)
+ax.tick_params(axis='both',which='major',labelsize=16)
+fig.autofmt_xdate()
+plt.show()
